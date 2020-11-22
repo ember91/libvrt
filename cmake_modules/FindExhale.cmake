0760a270a9620f60eafc42e5c561324ac21404f0
Emil Berg <emil.berg1991@gmail.com>
2020-11-22 14:03:49 +0100

    Add FindExhale.cmake

---
+++ cmake_modules/FindExhale.cmake
@@ -0,0 +1,13 @@
+find_package(PythonInterp)
+if(PYTHONINTERP_FOUND)
+  execute_process(
+    COMMAND
+      ${PYTHON_EXECUTABLE} -c
+      "from importlib import util;print(util.find_spec('exhale') is not None)"
+    OUTPUT_VARIABLE EXHALE_IMPORT_STATUS
+    ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+endif(PYTHONINTERP_FOUND)
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(Exhale DEFAULT_MSG EXHALE_IMPORT_STATUS)
