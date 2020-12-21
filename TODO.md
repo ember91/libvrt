ebadb4cc7b154551bbccff0009b2c727fcb4242c
Emil Berg <emil.berg1991@gmail.com>
2020-12-21 10:12:18 +0100

    Add time difference example

--- TODO.md
+++ TODO.md
@@ -5,7 +5,9 @@
   * Handle leap seconds for UTC and not for UTC.
   * Perhaps also with extra time zone information.
 * Add uninstall option to CMake
-* Add example with time.h
+* Add missing vrt_string error codes
+* Change name of p1 and p2 in vrt_time documentation
+* CMake find clang-tidy, iwyu
 
 # Future
 * Add single header library?
---
+++ example/src/time_difference.c
@@ -0,0 +1,49 @@
+/*
+ * Calculate time difference between two packets.
+ */
+
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <vrt/vrt_init.h>
+#include <vrt/vrt_string.h>
+#include <vrt/vrt_time.h>
+#include <vrt/vrt_types.h>
+
+/* Sample rate [Hz] */
+#define SAMPLE_RATE 16e6
+
+int main() {
+    /* Initialize to reasonable values */
+    struct vrt_packet p1;
+    struct vrt_packet p2;
+    vrt_init_packet(&p1);
+    vrt_init_packet(&p2);
+
+    /* Configure packet 1 */
+    p1.header.tsi                          = VRT_TSI_UTC;
+    p1.header.tsf                          = VRT_TSF_SAMPLE_COUNT;
+    p1.fields.integer_seconds_timestamp    = 5;
+    p1.fields.fractional_seconds_timestamp = 7000;
+
+    /* Configure packet 2 */
+    p2.header.tsi                          = VRT_TSI_UTC;
+    p2.header.tsf                          = VRT_TSF_SAMPLE_COUNT;
+    p2.fields.integer_seconds_timestamp    = 8;
+    p2.fields.fractional_seconds_timestamp = 9000;
+
+    /* Calculate time difference */
+    struct vrt_time diff;
+    int32_t         rv = vrt_time_difference(&p2, &p1, SAMPLE_RATE, &diff);
+    if (rv < 0) {
+        fprintf(stderr, "Failed to write packet: %s\n", vrt_string_error(rv));
+        return EXIT_FAILURE;
+    }
+
+    /* Display time difference between packets */
+    printf("Time difference of %" PRIi32 " s, %" PRIu64 " ps\n", diff.s, diff.ps);
+
+    return EXIT_SUCCESS;
+}
